27d537795c72ffed9d8c210dff30e62a932204f7
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2022-03-19 19:20:58 +0700

    feat: google keep webview

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-  println!("Hello World!")
+use wry::{
+  application::{
+      event::{Event, StartCause, WindowEvent},
+      event_loop::{ControlFlow, EventLoop},
+      window::WindowBuilder,
+  },
+  webview::WebViewBuilder,
+};
+
+fn main() -> wry::Result<()> {
+  let event_loop = EventLoop::new();
+  let window = WindowBuilder::new()
+      .with_title("Google Keep")
+      .build(&event_loop)?;
+  let _webview = WebViewBuilder::new(window)?
+      .with_url("https://keep.google.com/")?
+      .build()?;
+
+  event_loop.run(move |event, _, control_flow| {
+      *control_flow = ControlFlow::Wait;
+
+      match event {
+          Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+          Event::WindowEvent {
+              event: WindowEvent::CloseRequested,
+              ..
+          } => *control_flow = ControlFlow::Exit,
+          _ => (),
+      }
+  });
 }
