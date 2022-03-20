1306cd3ffd64d1b992abe8127bc0a767193e23bc
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2022-03-20 21:30:50 +0700

    feat: window icon

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,50 @@
+extern crate image;
+use std::path::Path;
+
 use wry::{
-  application::{
-      event::{Event, StartCause, WindowEvent},
-      event_loop::{ControlFlow, EventLoop},
-      window::WindowBuilder,
-  },
-  webview::WebViewBuilder,
+    application::{
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::{Icon, WindowBuilder},
+    },
+    webview::WebViewBuilder,
 };
 
+fn load_icon(path: &Path) -> Icon {
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open(path)
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
+}
+
 fn main() -> wry::Result<()> {
-  let event_loop = EventLoop::new();
-  let window = WindowBuilder::new()
-      .with_title("Google Keep")
-      .build(&event_loop)?;
-  let _webview = WebViewBuilder::new(window)?
-      .with_url("https://keep.google.com/")?
-      .build()?;
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/static/icon.png");
+    let icon = load_icon(Path::new(path));
 
-  event_loop.run(move |event, _, control_flow| {
-      *control_flow = ControlFlow::Wait;
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Google Keep")
+        .with_window_icon(Some(icon))
+        .build(&event_loop)?;
+    let _webview = WebViewBuilder::new(window)?
+        .with_url("https://keep.google.com/")?
+        .build()?;
 
-      match event {
-          Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
-          Event::WindowEvent {
-              event: WindowEvent::CloseRequested,
-              ..
-          } => *control_flow = ControlFlow::Exit,
-          _ => (),
-      }
-  });
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
